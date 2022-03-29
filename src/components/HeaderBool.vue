47e0e1d7bf5bc184cc3dabbf4b327e481076a5c4
Marzio95 <marziodellarocca14@gmail.com>
2022-03-29 11:59:25 +0200

    layout base

---
+++ src/components/HeaderBool.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header p-4">
+    <h1>Boolflix</h1>
+    <div>
+      <input type="text" name="film" id="film" placeholder="Cerca film" />
+      <button>Cerca</button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderBool",
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+}
+h1 {
+  color: red;
+  text-transform: uppercase;
+}
+button {
+  background-color: red;
+  border: 1px solid black;
+  color: white;
+  height: 2.5rem;
+  width: 5rem;
+}
+input {
+  height: 2.5rem;
+  width: 20rem;
+}
+</style>
---
+++ src/components/MainBool.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="p-5">
+    <h3>titolo film</h3>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainBool",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+main {
+  height: calc(100vh - 80px);
+  background-color: grey;
+}
+</style>
