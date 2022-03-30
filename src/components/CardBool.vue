aea8f2f451c37db195296b8e02a4b72f8d694b67
Marzio95 <marziodellarocca14@gmail.com>
2022-03-30 12:03:49 +0200

    implemento le immagini e le stelle

--- src/components/CardBool.vue
+++ src/components/CardBool.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="card p-4 col-2 text-center text-white bg-black">
-    <h2>{{ filmData.title }}</h2>
-    <h3
+    <img
+      v-show="filmData.poster_path != null"
+      :src="'http://image.tmdb.org/t/p/w342/' + filmData.poster_path"
+      :alt="filmData.title"
+    />
+    <h3>{{ filmData.title }}</h3>
+    <h5
       v-show="
         filmData.title.toLowerCase() != filmData.original_title.toLowerCase()
       "
     >
       {{ filmData.original_title }}
-    </h3>
+    </h5>
     <lang-flag class="flag" :iso="filmData.original_language" squared="false" />
     <h4>{{ filmData.vote_average }}</h4>
+    <div>
+      <div class="star" v-for="element in fullArrayStars()" :key="element">
+        &starf;
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,11 +27,24 @@
 import LangFlag from "vue-lang-code-flags";
 export default {
   name: "CardBool",
+  data() {
+    return {
+      starf: "star",
+      arrayStars: [],
+      stars: Math.floor(this.filmData.vote_average / 2),
+    };
+  },
   components: {
     LangFlag,
   },
   props: {
     filmData: Object,
+  },
+  methods: {
+    fullArrayStars() {
+      console.log(this.stars);
+      return this.stars;
+    },
   },
 };
 </script>
@@ -32,6 +55,27 @@ export default {
   font-size: 2rem;
 }
 .card {
-  height: 20rem;
+  height: 23rem;
+  position: relative;
+  width: 290px;
+}
+img {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1312;
+}
+img:hover {
+  transition: 1s;
+  opacity: 0;
+}
+.star {
+  font-size: 2rem;
+  display: inline-block;
+  color: gold;
 }
 </style>
--- src/components/CardSerie.vue
+++ src/components/CardSerie.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="card p-4 col-2 text-center text-white bg-black">
+    <img
+      v-show="serieData.poster_path != null"
+      :src="'http://image.tmdb.org/t/p/w342/' + serieData.poster_path"
+      :alt="serieData.title"
+    />
     <h2>{{ serieData.name }}</h2>
     <h3
       v-show="
@@ -14,6 +19,11 @@
       :squared="false"
     />
     <h4>{{ serieData.vote_average }}</h4>
+    <div>
+      <div class="star" v-for="element in fullArrayStars()" :key="element">
+        &starf;
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,11 +31,24 @@
 import LangFlag from "vue-lang-code-flags";
 export default {
   name: "CardSerie",
+  data() {
+    return {
+      starf: "star",
+      arrayStars: [],
+      stars: Math.floor(this.serieData.vote_average / 2),
+    };
+  },
   components: {
     LangFlag,
   },
   props: {
     serieData: Object,
+  },
+  methods: {
+    fullArrayStars() {
+      console.log(this.stars);
+      return this.stars;
+    },
   },
 };
 </script>
@@ -36,6 +59,27 @@ export default {
   font-size: 2rem;
 }
 .card {
-  height: 20rem;
+  height: 23rem;
+  position: relative;
+  width: 290px;
+}
+img {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1312;
+}
+img:hover {
+  transition: 1s;
+  opacity: 0;
+}
+.star {
+  font-size: 2rem;
+  display: inline-block;
+  color: gold;
 }
 </style>
